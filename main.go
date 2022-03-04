1436899931357321143143ab8eb62ae38fb71b1e
fdsig <fdesigley@gmail.com>
2022-03-04 12:04:11 +0000

    none

--- main.go
+++ main.go
@@ -1,36 +1,27 @@
 package main
-import ("os"
- "fmt"
- "os/exec"
-)
 
-func main(){
+import "os"
+
+
+// docker run image <cmd> <params>
+// go run main.go run <cmd> <params>
+
+func main() {
 	switch os.Args[1] {
 	case "run":
-		run()
-
 	default:
 		panic("bad command")
-		
+
 	}
 }
 
+func run() {
 
-func run(){
-	
 	fmt.Printf("Running %v\n", os.Args[2:])
-
-	cmd:= exec.Command(os.Args[2], os.Args[3:]... )
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	
-	cmd.Run()
-
 }
 
-func must(err error){
-	if err != nil{
+func must(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
