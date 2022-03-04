5f802db68933a330a469742fea4a3c44754463cb
fdsig <fdesigley@gmail.com>
2022-03-04 12:22:51 +0000

    running code

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ using code from the amazing [Liz Rice](https://github.com/lizrice/containers-fro
 
 Becasue its always nice to learn new tools in their native language even if only at quite a surface level. 
 
-
+I copied out manually for my own leanring however origional code is available from linked repository above. 
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "os"
-
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 // docker run image <cmd> <params>
 // go run main.go run <cmd> <params>
@@ -9,6 +12,7 @@ import "os"
 func main() {
 	switch os.Args[1] {
 	case "run":
+		run()
 	default:
 		panic("bad command")
 
@@ -16,8 +20,16 @@ func main() {
 }
 
 func run() {
-
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
 }
 
 func must(err error) {
